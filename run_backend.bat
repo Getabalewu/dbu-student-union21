3327aa675747ba521318b2fabad9f34bb8f9afa5
fasikaw000 <aytenfasikaw21@gmail.com>
2026-05-01 14:54:12 +0300

    chore: initial commit and fix dev scripts

--- run_backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd dbu-student-union21-main\backend
-"C:\Program Files\nodejs\node.exe" server.js
-pause
--- run_frontend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd dbu-student-union21-main\project
-"C:\Program Files\nodejs\node.exe" node_modules\vite\bin\vite.js
-pause
